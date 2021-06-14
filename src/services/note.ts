375eb0bb567266bad14fbe2427e3d2c05fff1c4c
Wordless Echo <wordless@echo.moe>
2021-06-14 22:58:16 +0800

    feat(service): add note service
    Currently, note service only have a method `getAll()`.
    `getAll()` returns all notes as `note[]` type.

---
+++ src/services/note.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { Note } from '../types';
+
+const baseUrl = '/api/notes';
+let token: string | null = null;
+type Config = { headers: { Authorization: string } };
+
+const setToken = (newToken: string) => {
+  token = `bearer ${newToken}`;
+};
+
+const getConfig = (): Config => {
+  if (token === null) {
+    throw new TypeError('Token should not be null');
+  }
+
+  return { headers: { Authorization: token } };
+};
+
+const getAll = async () => {
+  const response = await axios.get<Note[]>(baseUrl, getConfig());
+  return response.data;
+};
+
+export default { setToken, getAll };
