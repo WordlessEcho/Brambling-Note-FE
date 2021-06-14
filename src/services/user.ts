d5c6da3788135fd8c9fecfe061a48ca25971ced2
Wordless Echo <wordless@echo.moe>
2021-06-14 19:51:24 +0800

    feat(service): add user service
    User service is a simple function with only a `create()` method.
    `create()` receives a NewUser type of string and returns a User type
    object.

---
+++ src/services/user.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import { NewUser, User } from '../types';
+
+const baseUrl = '/api/user';
+
+const create = async (newUser: NewUser) => {
+  const response = await axios.post<User>(baseUrl, newUser);
+  return response.data;
+};
+
+export default { create };
