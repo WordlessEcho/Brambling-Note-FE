0d8e32ea71d0329d7abd5cc9c2a7a4bdb148746a
Wordless Echo <wordless@echo.moe>
2021-06-15 21:57:32 +0800

    feat(core): add user management with React Hook
    - `handleLogout()`: Clear cache from browser, note service and React
    Hook.
    - Set user from React State Hook or browser cache by detecting user
    state change with `useEffect()`.
    - Add properties to ApplicationBar as its requirement (See f672b04).

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { Container, createMuiTheme, ThemeProvider } from '@material-ui/core';
+import { Container, ThemeProvider, createMuiTheme } from '@material-ui/core';
 import { zhCN } from '@material-ui/core/locale';
+
+import { User } from './types';
+import noteService from './services/note';
+import utils from './utils';
 
 import ApplicationBar from './components/ApplicationBar';
 
+type DialogStatus = { login: boolean };
 const theme = createMuiTheme({}, zhCN);
 
-const App = () => (
-  <ThemeProvider theme={theme}>
-    <ApplicationBar />
+const App = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [dialogStatus, setDialogStatus] = useState<DialogStatus>({ login: false });
 
-    <Container component="main">
-      Hello world!
-    </Container>
-  </ThemeProvider>
-);
+  const handleLogout = () => {
+    localStorage.removeItem('user');
+    noteService.clearToken();
+    setUser(null);
+  };
+
+  useEffect(() => {
+    if (user !== null) {
+      localStorage.setItem('user', JSON.stringify(user));
+      noteService.setToken(user.token);
+      // TODO: get all notes
+    } else {
+      const cacheUser = localStorage.getItem('user');
+
+      if (cacheUser !== null) {
+        const parsedUser = utils.toUser(JSON.parse(cacheUser));
+        setUser(parsedUser);
+        noteService.setToken(parsedUser.token);
+      }
+      // TODO: display a user guide
+    }
+  }, [user]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <ApplicationBar
+        handleLogout={handleLogout}
+        displayName={user === null ? null : user.name}
+        showLogin={() => setDialogStatus({ ...dialogStatus, login: true })}
+      />
+
+      <Container component="main">
+        Hello world!
+      </Container>
+    </ThemeProvider>
+  );
+};
 
 export default App;
