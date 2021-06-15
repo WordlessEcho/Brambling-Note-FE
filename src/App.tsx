1cec09160cccbeecf4c3beda1f51af1c55eeaa13
Wordless Echo <wordless@echo.moe>
2021-06-15 22:24:11 +0800

    feat(core): add `handleLogin()`
    `handleLogin()` is a simple function receives a LoginUser type object
    and set user in React Hook.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from 'react';
 import { Container, ThemeProvider, createMuiTheme } from '@material-ui/core';
 import { zhCN } from '@material-ui/core/locale';
 
-import { User } from './types';
+import { LoginUser, User } from './types';
+import loginService from './services/login';
 import noteService from './services/note';
 import utils from './utils';
 
@@ -15,6 +16,11 @@ const theme = createMuiTheme({}, zhCN);
 const App = () => {
   const [user, setUser] = useState<User | null>(null);
   const [dialogStatus, setDialogStatus] = useState<DialogStatus>({ login: false });
+
+  const handleLogin = (loginUser: LoginUser) => (
+    loginService.login(loginUser)
+      .then((u) => setUser(u))
+  );
 
   const handleLogout = () => {
     localStorage.removeItem('user');
