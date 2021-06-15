f672b046ca4d4a571b7b89334f5319c5bf703240
Wordless Echo <wordless@echo.moe>
2021-06-15 21:33:46 +0800

    feat(ui): add login and logout buttons
    - App name in `AppBar` is set to `flexGrow: 1` to make buttons to align
    right.
    
    - ApplicationBar now receive three params.
      - `displayName`: Not unique name of a user. Exists only user logined.
      - `showLogin`: Set `open` of `Login` Dialog to true.
      - `handleLogout`: Clear cache of notes and user info.

--- src/components/ApplicationBar.tsx
+++ src/components/ApplicationBar.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
+import {
+  Button, Typography, Toolbar, AppBar, makeStyles,
+} from '@material-ui/core';
 
-import { Typography, Toolbar, AppBar } from '@material-ui/core';
+type Props = {
+  displayName: string | null,
+  showLogin: () => void,
+  handleLogout: () => void,
+};
 
-const ApplicationBar = () => (
-  <AppBar position="static">
-    <Toolbar>
-      <Typography variant="h6">
-        便签
-      </Typography>
-    </Toolbar>
-  </AppBar>
-);
+const useStyles = makeStyles(() => ({
+  title: { flexGrow: 1 },
+}));
+
+const ApplicationBar = ({ displayName, showLogin, handleLogout }: Props) => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" className={classes.title}>
+          便签
+        </Typography>
+        {displayName === null
+          ? <Button color="inherit" onClick={showLogin}>登入</Button>
+          : <Button color="inherit" onClick={handleLogout}>{displayName}</Button>}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default ApplicationBar;
