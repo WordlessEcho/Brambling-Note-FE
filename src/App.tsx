4dd07a6d05f25c29ec6f0dea42052b92418ee307
Wordless Echo <wordless@echo.moe>
2021-06-20 20:11:24 +0800

    feat(core): error handling at undo
    Show a error while `noteToDelete` is null at undo.

--- src/App.tsx
+++ src/App.tsx
@@ -69,6 +69,8 @@ const App = () => {
   const resetSnackbar = (undo: boolean) => {
     if (undo && noteToDelete !== null) {
       setNotes(notes.concat(noteToDelete));
+    } else {
+      setErrorMessage({ title: '您要删除的便签已不存在', content: null });
     }
 
     setSnackbarAction(null);
