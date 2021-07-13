d89d0bb44bc42968599fb63f0e7b449bdaa52e55
WordlessEcho <wordless@echo.moe>
2021-07-13 12:40:23 +0800

    feat(ui): change style of snackbar to classical

--- src/components/NotificationSnackbar.tsx
+++ src/components/NotificationSnackbar.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {
-  Button, Slide, Snackbar, SlideProps,
+  Button, Slide, Snackbar, SlideProps, SnackbarContent, createStyles, Theme, makeStyles,
 } from '@material-ui/core';
 
 type Props = {
@@ -10,6 +10,21 @@ type Props = {
   actionUndo: (undo: boolean) => void,
 };
 type TransitionProps = Omit<SlideProps, 'direction'>;
+const useStyles = makeStyles((t: Theme) => createStyles({
+  snackbar: {
+    bottom: 0,
+    [t.breakpoints.down('xs')]: {
+      left: 0,
+      right: 0,
+    },
+  },
+  snackbarContent: {
+    borderRadius: 2,
+    [t.breakpoints.down('xs')]: {
+      borderRadius: 0,
+    },
+  },
+}));
 
 // https://material-ui.com/components/snackbars/#control-slide-direction
 // eslint-disable-next-line react/jsx-props-no-spreading
@@ -21,6 +36,7 @@ const NotificationSnackbar = ({
 }: Props) => {
   // keep message in exit animation
   const [cachedMessage, setCacheMessage] = useState(message);
+  const classes = useStyles();
 
   useEffect(() => {
     if (message !== null) {
@@ -34,17 +50,22 @@ const NotificationSnackbar = ({
   return (
     <Snackbar
       key={cachedMessage}
+      className={classes.snackbar}
       open={message !== null}
       onClose={action === null ? undefined : action}
       autoHideDuration={timeout}
       TransitionComponent={getTransistion}
-      message={cachedMessage}
-      action={(
-        <Button color="secondary" size="small" onClick={() => actionUndo(true)}>
-          撤销
-        </Button>
-      )}
-    />
+    >
+      <SnackbarContent
+        className={classes.snackbarContent}
+        message={cachedMessage}
+        action={(
+          <Button color="secondary" size="small" onClick={() => actionUndo(true)}>
+            撤销
+          </Button>
+        )}
+      />
+    </Snackbar>
   );
 };
 
